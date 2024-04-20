f5540a92f9995eed03c7358e3c292897ee830ebf
qhq <qhqcz@qq.com>
2024-04-20 23:30:08 +0800

    修复: 新建订单中UI未调整

--- OM功能扩展.user.js
+++ OM功能扩展.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         OM功能扩展
-// @version      20240420.2323
+// @version      20240420.2329
 // @description  OM系统功能调整优化
 // @author       Mr.Q
 // @namespace    https://greasyfork.org/users/9065
@@ -1355,6 +1355,7 @@
         }
 
         if (window.location.pathname.endsWith("/modules/repr_addOrderAction/navigate.jsp")) {
+            UnNormalOrder.modifyStyle();
             NormalOrder.modifyStyle();
             if (PopsPanel.getValue("autoSyncSAP")) {
                 NormalOrder.hiddenElements();
